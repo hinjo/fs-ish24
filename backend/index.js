49dcee5831005c9e4177ce0b86ebce40ead4310b
Vanja Ferhatovic <vanja.ferhatovic@gmail.com>
2024-04-29 14:26:40 +0200

    add first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require("express"),
+  path = require("path"),
+  cors = require("cors")
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(cors())
+app.use(express.static(path.join(path.resolve(), "dist")))
+
+app.get("/api", (_request, response) => {
+  response.send({ hello: "World!" })
+  //hämta data från db om vi så vill
+  // response.send("<html><head></head><body>Hello there</body></html>")
+})
+
+app.get("/hej", (_request, response) => {
+  response.send("hej på dig!!!!!")
+})
+
+app.get("/health", (_request, response) => {
+  response.status(200).send("OK")
+})
+
+app.listen(port, () => {
+  console.log(`Redo på http://localhost:${port}/`)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./App.css"
+import { useEffect } from "react"
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [result, setResult] = useState("")
+
+  useEffect(() => {
+    fetch("/api")
+      .then((response) => response.json())
+      .then((result) => {
+        setResult(result.hello)
+        // alert(`Hello ${result.hello}!`)
+      })
+  }, [])
+
+  return (
+    <>
+      <div>
+        Helloooo {result}
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
